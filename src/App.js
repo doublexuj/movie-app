02bb90e5387a8b9f361ecca0f3a70827d2daead3
Jeong Woojin <doublexuj@gmail.com>
2022-04-03 20:58:19 +0900

    Add Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/ticker")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>Welcome!</h1>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin, index) => (
+            <li key={index}>
+              {coin.name} ({coin.symbol}): ${coin.price_usd} USD
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
